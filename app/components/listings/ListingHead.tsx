ae999c7030d0b7859b61bcdc6a5516e1459e8094
AleksandarSapic <shapke10@gmail.com>
2024-05-09 18:32:35 +0200

    Created property page

---
+++ app/components/listings/ListingHead.tsx
@@ -0,0 +1,57 @@
+import HeartButton from "@/app/components/HeartButton";
+import Heading from "@/app/components/Heading";
+import useCountries from "@/app/hooks/useCountries";
+import React from "react";
+import {SafeUser} from "@/app/types";
+import Image from "next/image";
+
+interface ListingHeadProps {
+    title: string;
+    locationValue: string;
+    imageSrc: string;
+    id: string;
+    currentUser?: SafeUser | null;
+}
+
+const ListingHead: React.FC<ListingHeadProps> = ({
+                                                     title,
+                                                     locationValue,
+                                                     imageSrc,
+                                                     id,
+                                                     currentUser
+                                                 }) => {
+    const {getByValue} = useCountries();
+
+    const location = getByValue(locationValue);
+
+    return (
+        <>
+            <Heading
+                title={title}
+                subtitle={`${location?.region}, ${location?.label}`}
+            />
+            <div className={
+                "w-full " +
+                "h-[60vh] " +
+                "overflow-hidden " +
+                "rounded-xl " +
+                "relative"
+            }>
+                <Image
+                    fill
+                    className={"object-cover w-full"}
+                    src={imageSrc}
+                    alt={"Image"}
+                />
+                <div className={"absolute top-5 right-5"}>
+                    <HeartButton
+                        listingId={id}
+                        currentUser={currentUser}
+                    />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default ListingHead;
